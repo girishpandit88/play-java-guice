4a4183b29c249d30b4a73aaaffaf642968600f3f
girishpandit88 <girschol7@gmail.com>
2014-06-30 21:56:10 -0700

    Fix bucket value - dont get the enclosing quotes, use textValue() instead of String.valueof()

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,56 +1,49 @@
 package controllers;
 
-import java.util.List;
-import java.util.Map;
-
+import com.amazonaws.services.s3.model.S3ObjectSummary;
+import com.amazonaws.util.json.JSONException;
+import com.amazonaws.util.json.JSONObject;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.google.inject.Inject;
 import org.slf4j.LoggerFactory;
-
-import play.Configuration;
-import play.Play;
-import play.data.DynamicForm;
-import play.data.Form;
 import play.mvc.Controller;
 import play.mvc.Result;
 import services.S3Service;
 
-import com.amazonaws.services.s3.model.S3ObjectSummary;
-import com.amazonaws.util.json.JSONException;
-import com.amazonaws.util.json.JSONObject;
-import com.google.inject.Inject;
+import java.util.List;
 
 public class Application extends Controller {
 
 	private static final org.slf4j.Logger logger = LoggerFactory
 			.getLogger(Application.class);
+//    private static Configuration config = Play.application().configuration();
+    @Inject
+    private S3Service s3Service;
 
-	@Inject
-	private S3Service s3Service;
-	private static Configuration config = Play.application().configuration();
-	static String S3_BUCKET = config.getString("aws.s3.bucket");
+//    private static Result error() {
+//        return internalServerError("s3Service null");
+//    }
 
 	public Result index() {
-		return ok(views.html.index.render("Welcome to my first app"));
-	}
+        return ok(views.html.index.render("Welcome to my first app"));
+    }
 
-	public Result getBucketContents() throws JSONException {
+    public Result getBucketContents() throws JSONException {
         JsonNode params = request().body().asJson();
-        String bucketName = params.get("bucketName").toString();
-        String didMap = String.valueOf(params.get("didMap"));
-        System.out.println("!#!@!@#!@#----------"+bucketName);
+        String bucketName = params.get("bucketName").textValue();
+        String didMap = params.get("nestedJSON").toString();
+
+        System.out.println("!#!@!@#!@#----------" + bucketName);
         logger.info("######" + bucketName);
-		JSONObject jsonResult = new JSONObject();
-        System.out.println("######"+ didMap);
+        JSONObject jsonResult = new JSONObject();
+        System.out.println("######" + didMap);
+        jsonResult.append("didMap from request", new JSONObject(didMap));
         List<S3ObjectSummary> files = s3Service.listBucketContents(bucketName);
-		for (S3ObjectSummary file : files) {
-			jsonResult.append(file.getKey(), file.getSize());
-		}
+        for (S3ObjectSummary file : files) {
+            jsonResult.append(file.getKey(), file.getSize());
+        }
 
-		return ok(jsonResult.toString());
-	}
-
-	private static Result error() {
-		return internalServerError("s3Service null");
-	}
+        return ok(jsonResult.toString());
+    }
 
 }
